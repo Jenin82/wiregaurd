668dfe0bd4fbb72ff767ebf7cc2c1ca47326ec78
rohittp0 <rohittp@Rohits-MacBook-Pro.local>
2025-10-23 00:12:34 +0530

    build output

---
+++ dist/cleanup.js
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+async function cleanup() {
+    try {
+        const iface = core.getState("iface") || "wg0";
+        // Try to tear down; don't fail the whole job if this errors
+        await exec.exec("sudo", ["wg-quick", "down", iface], { ignoreReturnCode: true });
+        core.info(`WireGuard interface '${iface}' is down.`);
+    }
+    catch (err) {
+        core.warning(`WireGuard teardown had issues: ${err?.message || err}`);
+    }
+}
+cleanup().then();
+//# sourceMappingURL=cleanup.js.map
